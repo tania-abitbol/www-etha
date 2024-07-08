f62532c1c416820da61404b1a6371b2b1a75d0a6
tania-abitbol <taniabitbol@gmail.com>
2024-07-08 10:26:24 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/redirect",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; frame-ancestors 'self';",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
